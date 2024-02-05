6eb8220fc69c15917538448085b883a12fe7de8d
paulkyev <paul.saire.contractor@bbva.com>
2024-02-05 09:19:08 -0500

    PERAR-26255 PERAR-26466 feat: fix sonar Que add privatre constructor

--- artifact/libraries/RBVDR406IMPL/src/main/java/com/bbva/rbvd/lib/r406/util/Constants.java
+++ artifact/libraries/RBVDR406IMPL/src/main/java/com/bbva/rbvd/lib/r406/util/Constants.java
@@ -2,5 +2,8 @@ package com.bbva.rbvd.lib.r406.util;
 
 public class Constants {
 
+    private Constants() {
+    }
+
     public static final String FLAG_RIMAC_LIST_QUOTATION = "rbvd.rimac.mock.list.quotation";
 }
